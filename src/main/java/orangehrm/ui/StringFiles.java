3765c03daae2cdb61d88bf08b27a792b6bf57a3b
sudhakarMonika <s.monika5426@gmail.com>
2026-01-01 19:36:44 +0530

    message here

---
+++ src/main/java/orangehrm/ui/StringFiles.java
@@ -0,0 +1,11 @@
+package orangehrm.ui;
+
+public class StringFiles {
+    // XPath locators
+    public static final String username = "//input[@name='username']";
+    public static final String pass = "//input[@name='password']";
+    public static final String loginbutton = "//button[@type='submit']";
+
+    // Wait time in seconds
+    public static final int waitTime = 10;
+}
